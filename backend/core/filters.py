36dbbc5db6a80d47943082115e4b70ea968baf06
Andrew Fedorchenko <furturnax@gmail.com>
2024-01-28 00:06:01 +0300

    Fix filters

--- backend/core/filters.py
+++ backend/core/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework as filters
 
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(filters.FilterSet):
@@ -21,8 +21,10 @@ class RecipeFilter(filters.FilterSet):
     author = filters.CharFilter(
         field_name='author__username'
     )
-    tags = filters.CharFilter(
-        field_name='tags__slug'
+    tags = filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name='tags__slug',
+        to_field_name='slug',
     )
     is_favorited = filters.BooleanFilter(
         method='get_is_favorited'
@@ -43,11 +45,11 @@ class RecipeFilter(filters.FilterSet):
     def get_is_favorited(self, queryset, name, value):
         """Проверяет наличие рецепта в избранном."""
         if value:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorite__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         """Проверяет наличие рецепта в списке покупок."""
         if value:
-            return queryset.filter(cart__user=self.request.user)
+            return queryset.filter(shoppingcart__user=self.request.user)
         return queryset
